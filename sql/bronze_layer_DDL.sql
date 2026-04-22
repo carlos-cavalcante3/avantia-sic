d8a9e77151919ff76e41be04ea053b6b72b1c552
Carlos Cavalcante Silva de Oliveira <carlos.cavalcante@avantia.com.br>
2026-04-22 10:45:20 -0300

    feat: adiciona pasta com sql DDL dos schema bronze e silver

---
+++ sql/bronze_layer_DDL.sql
@@ -0,0 +1,149 @@
+CREATE SCHEMA IF NOT EXISTS bronze;
+
+GRANT USAGE ON SCHEMA bronze TO anon, authenticated, service_role;
+
+
+# Caso haja mudanças sempre manter o drop table nas colunas antigas pra não haver conflitos
+
+DROP TABLE IF EXISTS bronze.deals CASCADE;
+DROP TABLE IF EXISTS bronze.contacts CASCADE;
+DROP TABLE IF EXISTS bronze.organizations CASCADE;
+DROP TABLE IF EXISTS bronze.pipelines CASCADE;
+DROP TABLE IF EXISTS bronze.tasks CASCADE;
+DROP TABLE IF EXISTS bronze.teams CASCADE;
+DROP TABLE IF EXISTS bronze.users CASCADE;
+
+CREATE TABLE bronze.users (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    phone TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE bronze.pipelines (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    "order" INTEGER,
+    stage_ids JSONB,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE bronze.teams (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    user_ids JSONB,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE bronze.organizations (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    owner_id TEXT,
+    url TEXT,
+    description TEXT,
+    segment_ids JSONB,
+    follower_ids JSONB,
+    custom_fields_cidade TEXT,
+    custom_fields_cnpj TEXT,
+    custom_fields_e_mail TEXT,
+    custom_fields_endereco TEXT,
+    custom_fields_estado TEXT,
+    custom_fields_inscricao_estadual TEXT,
+    custom_fields_nome_fantasia TEXT,
+    custom_fields_origem TEXT,
+    custom_fields_razao_social TEXT,
+    custom_fields_telefone TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE bronze.contacts (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    job_title TEXT,
+    emails JSONB,
+    phones JSONB,
+    organization_id TEXT,
+    context_origin TEXT,
+    social_profiles JSONB,
+    legal_bases JSONB,
+    custom_fields_celular TEXT,
+    custom_fields_sobrenome TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE bronze.deals (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    status TEXT,
+    total_price NUMERIC,
+    recurrence_price NUMERIC,
+    one_time_price NUMERIC,
+    expected_close_date TIMESTAMPTZ,
+    closed_at TIMESTAMPTZ,
+    pipeline_id TEXT,
+    stage_id TEXT,
+    owner_id TEXT,
+    organization_id TEXT,
+    lost_reason_id TEXT,
+    source_id TEXT,
+    campaign_id TEXT,
+    contact_ids JSONB,
+    rating INTEGER,
+    custom_fields_audio_e_video TEXT,
+    custom_fields_data_da_criacao TIMESTAMPTZ,
+    custom_fields_data_de_fechamento TIMESTAMPTZ,
+    custom_fields_data_de_origem_da_proposta TIMESTAMPTZ,
+    custom_fields_descricao TEXT,
+    custom_fields_margem TEXT,
+    custom_fields_motivo_da_perda TEXT,
+    custom_fields_prazo_do_contrato TEXT,
+    custom_fields_proposta_entregue_ao_cliente TEXT,
+    custom_fields_registro_de_oportunidade TEXT,
+    custom_fields_tipo_de_contrato TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE bronze.tasks (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    type TEXT,
+    status TEXT,
+    deal_id TEXT,
+    owner_ids JSONB,
+    due_date TIMESTAMPTZ,
+    completed_at TIMESTAMPTZ,
+    completed_by_id TEXT,
+    created_by_id TEXT,
+    description TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+
+CREATE INDEX idx_deals_updated_at ON bronze.deals(updated_at);
+CREATE INDEX idx_deals_organization_id ON bronze.deals(organization_id);
+CREATE INDEX idx_deals_pipeline_id ON bronze.deals(pipeline_id);
+
+CREATE INDEX idx_ontacts_updated_at ON bronze.contacts(updated_at);
+CREATE INDEX idx_contacts_organization_id ON bronze.contacts(organization_id);
+
+CREATE INDEX idx_organizations_updated_at ON bronze.organizations(updated_at);
+
+CREATE INDEX idx_tasks_updated_at ON bronze.tasks(updated_at);
+CREATE INDEX idx_tasks_deal_id ON bronze.tasks(deal_id);
+
+# Permissões
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA bronze TO anon, authenticated, service_role;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA bronze TO anon, authenticated, service_role;
+GRANT ALL PRIVILEGES ON ALL ROUTINES IN SCHEMA bronze TO anon, authenticated, service_role;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA bronze GRANT ALL ON TABLES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA bronze GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
---
+++ sql/silver_layer_DDL.sql
@@ -0,0 +1,99 @@
+CREATE SCHEMA IF NOT EXISTS silver;
+
+# Caso haja mudanças sempre manter o drop table nas colunas antigas pra não haver conflitos
+
+DROP TABLE IF EXISTS silver.deals CASCADE;
+DROP TABLE IF EXISTS silver.contacts CASCADE;
+DROP TABLE IF EXISTS silver.organizations CASCADE;
+DROP TABLE IF EXISTS silver.pipelines CASCADE;
+DROP TABLE IF EXISTS silver.tasks CASCADE;
+DROP TABLE IF EXISTS silver.teams CASCADE;
+DROP TABLE IF EXISTS silver.users CASCADE;
+
+CREATE TABLE silver.contacts (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    job_title TEXT,
+    email_principal TEXT,
+    telefone_principal TEXT,
+    organization_id TEXT,
+    context_origin TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE silver.organizations (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    owner_id TEXT,
+    segment_name TEXT,
+    custom_fields_cidade TEXT,
+    custom_fields_estado TEXT,
+    custom_fields_razao_social TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE silver.deals (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    status TEXT,
+    total_price NUMERIC,
+    expected_close_date TIMESTAMPTZ,
+    closed_at TIMESTAMPTZ,
+    pipeline_id TEXT,
+    stage_id TEXT,
+    owner_id TEXT,
+    organization_id TEXT,
+    lost_reason_id TEXT,
+    rating INTEGER,
+    custom_fields_tipo_de_contrato TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE silver.pipelines (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE silver.tasks (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    type TEXT,
+    status TEXT,
+    deal_id TEXT,
+    owner_ids TEXT,
+    due_date TIMESTAMPTZ,
+    completed_at TIMESTAMPTZ,
+    completed_by_id TEXT,
+    created_by_id TEXT,
+    description TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE silver.teams (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE silver.users (
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ
+);
+
+
+# Permissões
+
+GRANT USAGE ON SCHEMA silver TO anon, authenticated, service_role;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA silver TO anon, authenticated, service_role;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA silver TO anon, authenticated, service_role;
+GRANT ALL PRIVILEGES ON ALL ROUTINES IN SCHEMA silver TO anon, authenticated, service_role;
